498f107f5219bfa8a666c411a812a7fdd526afe2
akjeldsen <andreas@askproductions.no>
2017-10-20 22:28:56 +0200

    Safety save
    save 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,25 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## 1.0 - This function wil cache the inverse of a matrix. The goal is 
+## to speed up this process with caching.
 
 ## Write a short comment describing this function
+## Write the same format as the example code
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        ##changing from template from mean to inverse
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse, getinverse = getinverse)
+        ##Changed to the values for inverse
+        
 }
 
 
@@ -12,4 +27,5 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## also here using the example as the template
 }
